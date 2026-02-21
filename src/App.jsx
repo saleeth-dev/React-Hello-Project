68ca7a58a749046531f709f7dcb51e6b54e11709
S H Mahammed Saleeth <selith514@gmail.com>
2026-02-21 22:45:08 +0530

    Initial React project upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1 class="Heading">Hello World</h1>
+      <p>Lorem ipsum dolor sit amet consectetur.</p>
+      </div>
+  )
+}
+
+export default App;
