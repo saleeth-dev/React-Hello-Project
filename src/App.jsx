374911fecfbf69226fb8444c01315a3535db06f1
S H Mahammed Saleeth <selith514@gmail.com>
2026-02-23 22:27:15 +0530

    Fetch and display message from server
    Fetches a message from the server and displays it.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useEffect,useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState();
+  const getHello = async () => {
+    const responce = await fetch('http://localhost:5000');
+
+    const data = await responce.json();
+
+    console.log(data);
+
+    setMessage(data.message);
+  }
+  useEffect(() => {
+    getHello();
+  }, [])
   return (
     <div>
-      <h1 class="Heading">Hello World</h1>
+      <h1 className="Heading">Hello World</h1>
+      <h1>{message}</h1>
       <p>Lorem ipsum dolor sit amet consectetur.</p>
       </div>
   )
